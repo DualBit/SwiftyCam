06d828bf1da79ae7cb3428e2453245d3b44b5f27
Simone Gasparini <simone.gasparini@dualb.it>
2020-06-09 12:18:00 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,21 @@
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyCam"
+    name: "SwiftyCam",
+    products: [
+        .library(
+            name: "SwiftyCam",
+            targets: ["SwiftyCam"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "SwiftyCam",
+            path: "Source",
+            exclude: [
+                "DemoSwiftyCam"
+            ]
+        ),
+    ]
 )
